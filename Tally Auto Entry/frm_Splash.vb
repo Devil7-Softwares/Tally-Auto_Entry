45ca2ba62b82b14a60d7141b1adf5438b969f67b
Devil7DK <dineshthangavel47@gmail.com>
2018-08-23 16:49:02 +0530

    Add Splash Screen & Make Single Instanc Application

---
+++ Tally Auto Entry/frm_Splash.vb
@@ -0,0 +1,23 @@
+﻿Public Class frm_Splash
+    Sub New()
+        InitializeComponent()
+    End Sub
+
+    Public Overrides Sub ProcessCommand(ByVal cmd As System.Enum, ByVal arg As Object)
+        MyBase.ProcessCommand(cmd, arg)
+    End Sub
+
+    Public Enum SplashScreenCommand
+        SomeCommandId
+    End Enum
+
+    Private Sub Splash_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If My.Settings.Skin <> "" Then
+            Try
+                Me.Theme.LookAndFeel.SkinName = My.Settings.Skin
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+End Class
