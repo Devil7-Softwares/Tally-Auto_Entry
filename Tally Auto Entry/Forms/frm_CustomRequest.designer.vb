307d542198dfb9b0effe534b146d9800da329bf2
Devil7DK <dineshthangavel47@gmail.com>
2018-09-05 16:12:04 +0530

    Use FastColoredTextBox in Custom XML Request Form

--- Tally Auto Entry/Forms/frm_CustomRequest.designer.vb
+++ Tally Auto Entry/Forms/frm_CustomRequest.designer.vb
@@ -12,25 +12,26 @@ Partial Class frm_CustomRequest
     End Sub
 
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_CustomRequest))
         Me.MainContainer = New DevExpress.XtraEditors.SplitContainerControl()
         Me.grp_Request = New DevExpress.XtraEditors.GroupControl()
-        Me.grp_Response = New DevExpress.XtraEditors.GroupControl()
         Me.Panel_Controls = New DevExpress.XtraEditors.PanelControl()
         Me.btn_Send = New DevExpress.XtraEditors.SimpleButton()
-        Me.txt_Request = New DevExpress.XtraEditors.MemoEdit()
-        Me.txt_Response = New DevExpress.XtraEditors.MemoEdit()
+        Me.grp_Response = New DevExpress.XtraEditors.GroupControl()
         Me.ProgressPanel = New DevExpress.XtraWaitForm.ProgressPanel()
+        Me.txt_Request = New FastColoredTextBoxNS.FastColoredTextBox()
+        Me.txt_Response = New FastColoredTextBoxNS.FastColoredTextBox()
         CType(Me.MainContainer, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.MainContainer.SuspendLayout()
         CType(Me.grp_Request, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.grp_Request.SuspendLayout()
-        CType(Me.grp_Response, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.grp_Response.SuspendLayout()
         CType(Me.Panel_Controls, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel_Controls.SuspendLayout()
-        CType(Me.txt_Request.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.txt_Response.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.grp_Response, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grp_Response.SuspendLayout()
+        CType(Me.txt_Request, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txt_Response, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MainContainer
@@ -59,17 +60,6 @@ Partial Class frm_CustomRequest
         Me.grp_Request.TabIndex = 0
         Me.grp_Request.Text = "Request"
         '
-        'grp_Response
-        '
-        Me.grp_Response.Controls.Add(Me.ProgressPanel)
-        Me.grp_Response.Controls.Add(Me.txt_Response)
-        Me.grp_Response.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.grp_Response.Location = New System.Drawing.Point(0, 0)
-        Me.grp_Response.Name = "grp_Response"
-        Me.grp_Response.Size = New System.Drawing.Size(559, 208)
-        Me.grp_Response.TabIndex = 0
-        Me.grp_Response.Text = "Response"
-        '
         'Panel_Controls
         '
         Me.Panel_Controls.Controls.Add(Me.btn_Send)
@@ -88,21 +78,16 @@ Partial Class frm_CustomRequest
         Me.btn_Send.TabIndex = 0
         Me.btn_Send.Text = "Send Request"
         '
-        'txt_Request
+        'grp_Response
         '
-        Me.txt_Request.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.txt_Request.Location = New System.Drawing.Point(2, 20)
-        Me.txt_Request.Name = "txt_Request"
-        Me.txt_Request.Size = New System.Drawing.Size(555, 129)
-        Me.txt_Request.TabIndex = 0
-        '
-        'txt_Response
-        '
-        Me.txt_Response.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.txt_Response.Location = New System.Drawing.Point(2, 20)
-        Me.txt_Response.Name = "txt_Response"
-        Me.txt_Response.Size = New System.Drawing.Size(555, 186)
-        Me.txt_Response.TabIndex = 0
+        Me.grp_Response.Controls.Add(Me.ProgressPanel)
+        Me.grp_Response.Controls.Add(Me.txt_Response)
+        Me.grp_Response.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.grp_Response.Location = New System.Drawing.Point(0, 0)
+        Me.grp_Response.Name = "grp_Response"
+        Me.grp_Response.Size = New System.Drawing.Size(559, 208)
+        Me.grp_Response.TabIndex = 0
+        Me.grp_Response.Text = "Response"
         '
         'ProgressPanel
         '
@@ -116,6 +101,61 @@ Partial Class frm_CustomRequest
         Me.ProgressPanel.Size = New System.Drawing.Size(555, 186)
         Me.ProgressPanel.TabIndex = 1
         Me.ProgressPanel.Text = "ProgressPanel1"
+        '
+        'txt_Request
+        '
+        Me.txt_Request.AutoCompleteBracketsList = New Char() {Global.Microsoft.VisualBasic.ChrW(40), Global.Microsoft.VisualBasic.ChrW(41), Global.Microsoft.VisualBasic.ChrW(123), Global.Microsoft.VisualBasic.ChrW(125), Global.Microsoft.VisualBasic.ChrW(91), Global.Microsoft.VisualBasic.ChrW(93), Global.Microsoft.VisualBasic.ChrW(34), Global.Microsoft.VisualBasic.ChrW(34), Global.Microsoft.VisualBasic.ChrW(39), Global.Microsoft.VisualBasic.ChrW(39)}
+        Me.txt_Request.AutoIndentCharsPatterns = ""
+        Me.txt_Request.AutoScrollMinSize = New System.Drawing.Size(27, 14)
+        Me.txt_Request.BackBrush = Nothing
+        Me.txt_Request.CharHeight = 14
+        Me.txt_Request.CharWidth = 8
+        Me.txt_Request.CommentPrefix = Nothing
+        Me.txt_Request.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txt_Request.DisabledColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.txt_Request.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txt_Request.Font = New System.Drawing.Font("Courier New", 9.75!)
+        Me.txt_Request.IsReplaceMode = False
+        Me.txt_Request.Language = FastColoredTextBoxNS.Language.XML
+        Me.txt_Request.LeftBracket = Global.Microsoft.VisualBasic.ChrW(60)
+        Me.txt_Request.LeftBracket2 = Global.Microsoft.VisualBasic.ChrW(40)
+        Me.txt_Request.Location = New System.Drawing.Point(2, 20)
+        Me.txt_Request.Name = "txt_Request"
+        Me.txt_Request.Paddings = New System.Windows.Forms.Padding(0)
+        Me.txt_Request.RightBracket = Global.Microsoft.VisualBasic.ChrW(62)
+        Me.txt_Request.RightBracket2 = Global.Microsoft.VisualBasic.ChrW(41)
+        Me.txt_Request.SelectionColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txt_Request.ServiceColors = CType(resources.GetObject("txt_Request.ServiceColors"), FastColoredTextBoxNS.ServiceColors)
+        Me.txt_Request.Size = New System.Drawing.Size(555, 129)
+        Me.txt_Request.TabIndex = 0
+        Me.txt_Request.Zoom = 100
+        '
+        'txt_Response
+        '
+        Me.txt_Response.AutoCompleteBracketsList = New Char() {Global.Microsoft.VisualBasic.ChrW(40), Global.Microsoft.VisualBasic.ChrW(41), Global.Microsoft.VisualBasic.ChrW(123), Global.Microsoft.VisualBasic.ChrW(125), Global.Microsoft.VisualBasic.ChrW(91), Global.Microsoft.VisualBasic.ChrW(93), Global.Microsoft.VisualBasic.ChrW(34), Global.Microsoft.VisualBasic.ChrW(34), Global.Microsoft.VisualBasic.ChrW(39), Global.Microsoft.VisualBasic.ChrW(39)}
+        Me.txt_Response.AutoIndentCharsPatterns = ""
+        Me.txt_Response.AutoScrollMinSize = New System.Drawing.Size(27, 14)
+        Me.txt_Response.BackBrush = Nothing
+        Me.txt_Response.CharHeight = 14
+        Me.txt_Response.CharWidth = 8
+        Me.txt_Response.CommentPrefix = Nothing
+        Me.txt_Response.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txt_Response.DisabledColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.txt_Response.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txt_Response.IsReplaceMode = False
+        Me.txt_Response.Language = FastColoredTextBoxNS.Language.XML
+        Me.txt_Response.LeftBracket = Global.Microsoft.VisualBasic.ChrW(60)
+        Me.txt_Response.LeftBracket2 = Global.Microsoft.VisualBasic.ChrW(40)
+        Me.txt_Response.Location = New System.Drawing.Point(2, 20)
+        Me.txt_Response.Name = "txt_Response"
+        Me.txt_Response.Paddings = New System.Windows.Forms.Padding(0)
+        Me.txt_Response.RightBracket = Global.Microsoft.VisualBasic.ChrW(62)
+        Me.txt_Response.RightBracket2 = Global.Microsoft.VisualBasic.ChrW(41)
+        Me.txt_Response.SelectionColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txt_Response.ServiceColors = CType(resources.GetObject("txt_Response.ServiceColors"), FastColoredTextBoxNS.ServiceColors)
+        Me.txt_Response.Size = New System.Drawing.Size(555, 186)
+        Me.txt_Response.TabIndex = 2
+        Me.txt_Response.Zoom = 100
         '
         'frm_CustomRequest
         '
@@ -131,12 +171,12 @@ Partial Class frm_CustomRequest
         Me.MainContainer.ResumeLayout(False)
         CType(Me.grp_Request, System.ComponentModel.ISupportInitialize).EndInit()
         Me.grp_Request.ResumeLayout(False)
-        CType(Me.grp_Response, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.grp_Response.ResumeLayout(False)
         CType(Me.Panel_Controls, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel_Controls.ResumeLayout(False)
-        CType(Me.txt_Request.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.txt_Response.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.grp_Response, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grp_Response.ResumeLayout(False)
+        CType(Me.txt_Request, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txt_Response, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -144,9 +184,9 @@ Partial Class frm_CustomRequest
     Friend WithEvents MainContainer As DevExpress.XtraEditors.SplitContainerControl
     Friend WithEvents grp_Request As DevExpress.XtraEditors.GroupControl
     Friend WithEvents grp_Response As DevExpress.XtraEditors.GroupControl
-    Friend WithEvents txt_Request As DevExpress.XtraEditors.MemoEdit
     Friend WithEvents Panel_Controls As DevExpress.XtraEditors.PanelControl
     Friend WithEvents btn_Send As DevExpress.XtraEditors.SimpleButton
-    Friend WithEvents txt_Response As DevExpress.XtraEditors.MemoEdit
     Friend WithEvents ProgressPanel As DevExpress.XtraWaitForm.ProgressPanel
+    Friend WithEvents txt_Request As FastColoredTextBoxNS.FastColoredTextBox
+    Friend WithEvents txt_Response As FastColoredTextBoxNS.FastColoredTextBox
 End Class
